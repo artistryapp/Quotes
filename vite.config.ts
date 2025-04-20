2da3f8ccaca457c03cf1a202575d5bdef1a801e9
YXJ0aXN0cnlhcHA <135151645+artistryapp@users.noreply.github.com>
2025-04-20 13:41:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/quote-aura-canvas/' : '/',
+  base: mode === 'production' ? '/Quotes' : '/',
   server: {
     host: "::",
     port: 8080,
